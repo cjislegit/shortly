427f2e992abca9f6c9b007d3082a316a4ac8f0a2
Carlos Ramirez <carlosjramirez11@gmail.com>
2024-07-17 21:00:27 -0700

    Started styling desktop navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,11 +3,26 @@ import mobileLogo from '../assets/images/logo.svg';
 const NavBar = () => {
   return (
     <nav className='flex justify-between mt-[50px] items-center'>
-      <div>
-        <img className='h-auto w-[60%]' src={mobileLogo} alt='Shortly logo' />
+      <div className=' lg:flex lg:w-[50%] lg:gap-[40px]'>
+        <img
+          className='h-auto w-[50%] lg:w-[20%] lg:max-w-[95px]'
+          src={mobileLogo}
+          alt='Shortly logo'
+        />
+        <ul className='hidden lg:flex lg:gap-[30px] text-custom-gray'>
+          <li>Features</li>
+          <li>Pricing</li>
+          <li>Resources</li>
+        </ul>
       </div>
-      <div>
-        <i className='fa-solid fa-bars fa-lg text-custom-gray'></i>
+      <div className='text-custom-gray lg:w-[50%] lg:flex justify-end'>
+        <i className='fa-solid fa-bars fa-lg lg:hidden'></i>
+        <ul className='hidden lg:flex lg:gap-[30px] items-center'>
+          <li>Login</li>
+          <li className='bg-custom-cyan text-white px-[20px] py-[6px] rounded-3xl '>
+            Sign Up
+          </li>
+        </ul>
       </div>
     </nav>
   );
