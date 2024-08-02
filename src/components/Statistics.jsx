17da6cba55d6d95576a3e08691981ea7b7722d8f
Carlos Ramirez <carlosjramirez11@gmail.com>
2024-08-01 20:56:20 -0700

    Started styling the first div of the statistics component for mobile

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,10 +1,19 @@
 const Statistics = () => {
   return (
     <section>
-      <div className='bg-shorten-mobile'>
-        <form action=''>
-          <input type='text' name='link' id='link' />
-          <input type='submit' value='Shorten It!' />
+      <div className='bg-shorten-mobile  bg-right-top bg-no-repeat bg-dark-violet rounded-md'>
+        <form action='' className='flex flex-col px-[10%] py-[10%]'>
+          <input
+            className='rounded-md mb-[15px]'
+            type='text'
+            name='link'
+            id='link'
+          />
+          <input
+            className='rounded-md text-white bg-custom-cyan'
+            type='submit'
+            value='Shorten It!'
+          />
         </form>
       </div>
       <div></div>
