3fc1ae084e97094c01f8dc78f0ce424216c8a2b6
Carlos Ramirez <carlosjramirez11@gmail.com>
2024-08-06 12:26:39 -0700

    Added more content to the statistics div and added css to be able to move the form around freely

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,7 +1,7 @@
 const Statistics = () => {
   return (
-    <section className='bg-custom-gray w-[100%] px-[50px]'>
-      <div className='bg-shorten-mobile  bg-right-top bg-no-repeat bg-dark-violet rounded-md mt-[50px]'>
+    <section className='bg-grayish-violet w-[100%] px-[50px] relative'>
+      <div className='bg-shorten-mobile  bg-right-top bg-no-repeat bg-dark-violet rounded-md mt-[50px] relative'>
         <form action='' className='flex flex-col px-[10%] py-[10%]'>
           <input
             className='rounded-md mb-[15px] h-[35px] pl-[15px]'
@@ -17,7 +17,13 @@ const Statistics = () => {
           />
         </form>
       </div>
-      <div></div>
+      <div className='text-center'>
+        <h2 className='poppins-bold text-2xl mb-[20px]'>Advanced Statistics</h2>
+        <p className='text-custom-gray'>
+          Track how your links are performing across the web with our advanced
+          statistics dashboard
+        </p>
+      </div>
       <div></div>
       <div></div>
       <div></div>
