5d8f6e67690736c21c885b9a457d15a99fd61736
Carlos Ramirez <carlosjramirez11@gmail.com>
2024-08-09 12:02:57 -0700

    Added additonal styling to the brand recogntion section for mobile

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,3 +1,7 @@
+import brandRecognition from '../assets/images/icon-brand-recognition.svg';
+import detailedRecords from '../assets/images/icon-detailed-records.svg';
+import fullyCustomizable from '../assets/images/icon-fully-customizable.svg';
+
 const Statistics = () => {
   return (
     <section className='bg-grayish-violet w-[100%] px-[50px] relative mt-[150px] text-center'>
@@ -24,16 +28,22 @@ const Statistics = () => {
           statistics dashboard
         </p>
       </div>
-      <div>
-        <img src='' alt='' />
+      <div className='flex flex-col bg-white relative mt-[55px] w-[100%]'>
+        <div className='bg-dark-violet rounded-full p-[15px] w-[70px] h-[70px] flex justify-center self-center relative top-[-30px]'>
+          <img
+            src={brandRecognition}
+            alt='Brand recognition icon'
+            className='h-[40px]'
+          />
+        </div>
         <h3 className='poppins-bold text-1xl mb-[20px]'>Brand Recognition</h3>
         <p>
-          Boost your brand recognition with each click. Generic links don ti
-          mean a thing. Branded links help instil confidence in your content.
+          Boost your brand recognition with each click. Generic links don't mean
+          a thing. Branded links help instil confidence in your content.
         </p>
       </div>
       <div>
-        <img src='' alt='' />
+        <img src={detailedRecords} alt='Detailed records icon' />
         <h3 className='poppins-bold text-1xl mb-[20px]'>Detailed Records</h3>
         <p>
           Gain insights into who is clicking your links. Knowing when and where
@@ -41,7 +51,7 @@ const Statistics = () => {
         </p>
       </div>
       <div>
-        <img src='' alt='' />
+        <img src={fullyCustomizable} alt='Fully customizable icon' />
         <h3 className='poppins-bold text-1xl mb-[20px]'>Fully Customizable</h3>
         <p>
           Improve brand awareness and content discoverability through
