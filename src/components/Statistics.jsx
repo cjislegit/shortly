5469e8982bc2199842e9682f78f9fd417f3e4f44
Carlos Ramirez <carlosjramirez11@gmail.com>
2024-08-09 18:00:02 -0700

    Changed the background color of the statistics div and added styling to all the divs inside of it

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -4,7 +4,7 @@ import fullyCustomizable from '../assets/images/icon-fully-customizable.svg';
 
 const Statistics = () => {
   return (
-    <section className='bg-grayish-violet w-[100%] px-[50px] relative mt-[150px] text-center'>
+    <section className='bg-gray-100 w-[100%] px-[50px] relative mt-[150px] text-center'>
       <div className='bg-shorten-mobile  bg-right-top bg-no-repeat bg-dark-violet rounded-md mt-[50px] relative top-[-70px]'>
         <form action='' className='flex flex-col px-[10%] py-[10%]'>
           <input
@@ -22,13 +22,15 @@ const Statistics = () => {
         </form>
       </div>
       <div>
-        <h2 className='poppins-bold text-2xl mb-[20px]'>Advanced Statistics</h2>
-        <p className='text-custom-gray'>
+        <h2 className='poppins-bold text-[1.7em] mb-[20px]'>
+          Advanced Statistics
+        </h2>
+        <p className='text-custom-gray poppins-medium'>
           Track how your links are performing across the web with our advanced
           statistics dashboard
         </p>
       </div>
-      <div className='flex flex-col bg-white relative mt-[55px] w-[100%]'>
+      <div className='flex flex-col bg-white relative mt-[55px] w-[100%] rounded-md px-5 pb-7'>
         <div className='bg-dark-violet rounded-full p-[15px] w-[70px] h-[70px] flex justify-center self-center relative top-[-30px]'>
           <img
             src={brandRecognition}
@@ -36,24 +38,34 @@ const Statistics = () => {
             className='h-[40px]'
           />
         </div>
-        <h3 className='poppins-bold text-1xl mb-[20px]'>Brand Recognition</h3>
-        <p>
+        <h3 className='poppins-bold text-[1.4em] mb-[20px]'>
+          Brand Recognition
+        </h3>
+        <p className='text-custom-gray poppins-medium'>
           Boost your brand recognition with each click. Generic links don't mean
           a thing. Branded links help instil confidence in your content.
         </p>
       </div>
-      <div>
-        <img src={detailedRecords} alt='Detailed records icon' />
-        <h3 className='poppins-bold text-1xl mb-[20px]'>Detailed Records</h3>
-        <p>
+      <div className='flex flex-col bg-white relative mt-[55px] w-[100%] rounded-md px-5 pb-7'>
+        <div className='bg-dark-violet rounded-full p-[15px] w-[70px] h-[70px] flex justify-center self-center relative top-[-30px]'>
+          <img src={detailedRecords} alt='Detailed records icon' />
+        </div>
+        <h3 className='poppins-bold text-[1.4em] mb-[20px]'>
+          Detailed Records
+        </h3>
+        <p className='text-custom-gray poppins-medium'>
           Gain insights into who is clicking your links. Knowing when and where
           people engage with your content helps inform better decisions.
         </p>
       </div>
-      <div>
-        <img src={fullyCustomizable} alt='Fully customizable icon' />
-        <h3 className='poppins-bold text-1xl mb-[20px]'>Fully Customizable</h3>
-        <p>
+      <div className='flex flex-col bg-white relative mt-[55px] w-[100%] rounded-md px-5 pb-7'>
+        <div className='bg-dark-violet rounded-full p-[15px] w-[70px] h-[70px] flex justify-center self-center relative top-[-30px]'>
+          <img src={fullyCustomizable} alt='Fully customizable icon' />
+        </div>
+        <h3 className='poppins-bold text-[1.4em] mb-[20px]'>
+          Fully Customizable
+        </h3>
+        <p className='text-custom-gray poppins-medium'>
           Improve brand awareness and content discoverability through
           customizable links, supercharging audience engagement.
         </p>
