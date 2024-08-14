3fd2cbb392cdd4c4c63a5ac9aecf11102d07d44b
Carlos Ramirez <carlosjramirez11@gmail.com>
2024-08-14 11:18:39 -0700

    Updated vite config files to open firefox

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import open from 'open';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    open: false,
+  },
+  plugins: [
+    react(),
+    {
+      name: 'custom-open',
+      apply: 'serve',
+      configureServer(server) {
+        server.httpServer.once('listening', () => {
+          const port = server.config.server.port || 3000;
+          const protocol = server.config.server.https ? 'https' : 'http';
+          const url = `${protocol}://localhost:${port}`;
+
+          open(url, { app: { name: 'firefox' } });
+        });
+      },
+    },
+  ],
+});
